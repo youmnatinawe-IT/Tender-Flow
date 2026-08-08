9c3ad60727b90f7ec2552a6c1b705d31b6c0ecf1
Youmna <youmnatinawe@gmail.com>
2026-08-08 15:06:43 +0300

    Update vite config and path casings

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs.plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // تجاهل تحذيرات واستدعاءات الملفات المتشابهة
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') return;
+        warn(warning);
+      },
+    },
+  },
 })
